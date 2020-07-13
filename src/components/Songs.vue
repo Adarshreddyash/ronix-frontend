61a8ed0e94c593deb738ba0502b1094ab62c8b17
adarshreddyash <adarshreddy9849@gmail>
2020-07-14 00:58:20 +0530

    songs update

--- src/components/Songs.vue
+++ src/components/Songs.vue
@@ -1,57 +1,45 @@
-<template lang="html">
-<ul v-if="posts && posts.length">
-    <v-li v-for="post in posts" :key="post.title">  
-          <v-flex md3>
-          <v-card
-           max-width="300"
-          class="ma-3"
-          >
-            <v-img
-              :src="post.album"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
-            >
-              <v-card-title v-text="post.title"></v-card-title>
-            </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-            </v-card>
-  </v-flex>
-            <v-snackbar
-      v-model="snackbar"
-      :timeout="timeout"
+<template>
+<v-container>
+    <v-layout wrap>
+      <v-flex xs4 md2
+        v-for="(item, index) in posts"
+        :key="index"
+        mb-16>
+        <v-card
+    class="mx-auto"
+    max-width="100"
+    max-height="90"
+  >
+    <v-img
+      class="white--text align-end rounded-lg"
+      height="110px"
+      width="120px"
+      :src="item.album"
+      
     >
-    <vuetify-audio :file="post.song" color="success" :ended="audioFinish"></vuetify-audio>
-
-      <template v-slot:action="{ attrs }">
-        <v-btn
-          color="blue"
-          text
-          v-bind="attrs"
-          @click="snackbar = false"
-        >
-          Close
-        </v-btn>
-      </template>
+    <v-btn class="ma-2" text icon color="grey">
+        <v-icon  @click="snackbar = true">mdi-play</v-icon>
+      </v-btn>
+    </v-img>
+    <v-snackbar
+      v-model="snackbar"
+      :multi-line="multiLine"
+       :timeout="-1"
+       light
+    >
+  <vue-plyr>
+    <audio>
+        <source :src="item.song" type="audio/mp3" />
+    </audio>
+</vue-plyr>
     </v-snackbar>
+ <div class="overflow_prevent">{{ item.title}}</div>
+    <div class="caption overflow_prevent" style="line-height: 100%;">{{ item.artist}}</div>
+  </v-card>
 
-    </v-li>
-  </ul>
+      </v-flex>
+  </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -63,11 +51,12 @@ export default {
       posts: [],
       errors: [],
       file: '',
-      snackbar:false,
+      multiLine: true,
+      snackbar: true,
     }
   },
   components: {
-        VuetifyAudio: () => import('vuetify-audio'),
+        // VuetifyAudio: () => import('vuetify-audio'),
     },
 
 mounted() {
@@ -102,10 +91,13 @@ mounted() {
 </script>
 
 <style lang="css">
-ul
-{
-	list-style: none;
+.overflow_prevent{
+   white-space: nowrap; 
+  overflow: hidden;
+  text-overflow: ellipsis; 
 }
 
-
+.plyr--full-ui input[type=range]{
+  color: #9921e8!important;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import VuePlyr from 'vue-plyr'
 
 Vue.config.productionTip = false
+
+Vue.use(VuePlyr, {
+  plyr: {
+    fullscreen: { enabled: false }
+  },
+  emit: ['ended']
+})
 
 new Vue({
   store,
