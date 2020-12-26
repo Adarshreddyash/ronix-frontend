928dedc7c865ac37baf656dc6e2f8c6e13c03f88
adarshreddyash <adarshreddy9849@gmail>
2020-12-26 22:16:55 +0530

    fixed bugs

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,12 @@
     <v-app-bar
       app
       dense
+      color="white"
     >
-      <div class="d-flex align-center">
+      <div >
           <v-img
-          class="mx-2"
-          src="https://i.imgur.com/qgGY4tB.png"
-          max-height="40"
-          max-width="40"
+          src="./assets/Ronixlogo.png"
+          max-width="90"
           contain
           ></v-img>
       </div>
@@ -27,6 +26,16 @@
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
+
+      <v-btn icon>
+         <v-badge
+          color="green"
+          content="3"
+          overlap
+        >
+        <v-icon>mdi-bell</v-icon>
+        </v-badge>
+      </v-btn>
   
   <div v-show="logus">
    <v-menu offset-y>
@@ -39,7 +48,7 @@
         <v-avatar size="27" v-if="$store.state.authUser.avatar">
           <img icon :src="$store.state.authUser.avatar" alt="Profile avatar">
         </v-avatar>
-
+    
         <v-avatar size="27" v-else>
           <img icon src="https://image.flaticon.com/icons/png/128/483/483361.png" alt="Profile avatar">
         </v-avatar>
@@ -52,6 +61,18 @@
         <v-list-item
         >
          <v-list-item-content>
+           <v-btn
+        color="green"
+        x-small
+        outlined
+        :loading="isSelecting"
+        @click="onButtonClick"
+        style="max-width:140px!important"
+      >
+        <v-icon left>mdi-cloud-upload-outline</v-icon>
+        upload picture
+      </v-btn>
+      <input type="file" accept="image/*" @change="uploadImage($event)" id="file-input">
               <v-list-item-title><v-icon class="ma-1">mdi-account-circle</v-icon>{{ userDetails.profile_name }}</v-list-item-title>
                <v-divider></v-divider>
               <v-list-item-subtitle><v-icon class="ma-1">mdi-email-check</v-icon>{{ userDetails.email }}</v-list-item-subtitle>
@@ -75,6 +96,7 @@
   </v-app>
 </template>
 <script>
+import axios from 'axios'
 
 export default {
   name: 'App',
@@ -94,10 +116,53 @@ export default {
         'Contact Us',
       ],
       isLoggedin: false,
+      defaultButtonText: 'upload',
+      selectedFile: null,
+      isSelecting: false
       }
   },
 
   methods:{
+
+    onButtonClick() {
+      this.isSelecting = true
+      window.addEventListener('focus', () => {
+        this.isSelecting = false
+      }, { once: true })
+
+      this.$refs.uploader.click()
+    },
+    onFileChanged(e) {
+      let formData = new FormData();
+      formData.append('avatar', e.target.files[0]);
+      
+      const base = {
+            baseURL: this.$store.state.endpoints.baseUrl,
+            headers: {
+            // Set your Authorization to 'JWT', not Bearer!!!
+              Authorization: `JWT ${this.$store.state.jwt}`,
+              'Content-Type': 'multipart/form-data'
+            },
+            xhrFields: {
+                withCredentials: true
+            }
+          }
+      const axiosInstance = axios.create(base)
+          axiosInstance({
+            url: "http://127.0.0.1:8000/rest-auth/user/",
+            formData,
+            method: "patch",
+          })
+      .then((response) => {
+              console.log(response.data)
+            })
+      .catch((error) => {
+          console.log(error);
+          console.debug(error);
+          console.dir(error);
+        })
+
+    }
   },
   
   computed:{
@@ -115,5 +180,21 @@ export default {
 <style lang="css">
 .v-snack__wrapper.theme--dark{
   background-color:#333333c7!important;
+}
+
+/* Medium Devices, Desktops */
+    @media only screen and (min-width : 992px) {
+
+  .v-toolbar__content, .v-toolbar__extension {
+    padding: 4px 136px!important;
+    }
+    }
+   .v-image__image--contain {
+    background-size: contain;
+    background-position: left!important;
+}
+
+.v-icon--left {
+  margin-right: 8px;
 }
 </style>
